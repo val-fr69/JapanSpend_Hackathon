438e20c2f052893a41896bd152e3e71d65f50844
Mouhamed <mouhamediop114@gmail.com>
2023-05-11 19:13:09 +0200

    create tab nav

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Model\RestaurantManager;
+use App\Model\ActivityManager;
+
 
 class HomeController extends AbstractController
 {
@@ -11,6 +13,11 @@ class HomeController extends AbstractController
         $restaurantManager = new RestaurantManager();
         $restaurant = $restaurantManager->selectAll();
 
-        return $this->twig->render('Home/index.html.twig', ['restaurants' => $restaurant]);
+        $activityManager = new ActivityManager();
+        $activity = $activityManager->selectAll();
+
+        return $this->twig->render('Home/index.html.twig',
+         ['restaurants' => $restaurant,
+         'activities' => $activity]);
     }
 }

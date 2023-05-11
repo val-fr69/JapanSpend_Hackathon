fbecf408489228a74f777c9b6c6ab86a202ccdc5
val-fr69 <[valentin.inacio@live.fr](mailto:valentin.inacio@live.fr)>
2023-05-11 18:52:01 +0200

    city

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,4 +14,12 @@ class HomeController extends AbstractController
 
         return $this->twig->render('Home/index.html.twig', ['restaurants' => $restaurant]);
     }
+
+    public function home(): string
+    {
+        $restaurantManager = new RestaurantManager();
+        $restaurant = $restaurantManager->selectAll();
+
+        return $this->twig->render('Home/home.html.twig');
+    }
 }
